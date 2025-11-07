fd8d732c437bc150f285df835457230e8b9a7076
chickenchi <herosofthestom12@gmail.com>
2025-11-07 21:39:13 +0900

    Add :: 페이지 리다이렉션 및 styled-components 추가

---
+++ crossword/app/crossword/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import styled from "styled-components";
+
+const CrosswordDiv = styled.div``;
+const CrosswordTable = styled.table`
+  border: 1px solid white;
+  border-collapse: collapse;
+`;
+const CrosswordColumn = styled.tr`
+  border: 1px solid white;
+`;
+const CrosswordRow = styled.td`
+  border: 1px solid white;
+`;
+
+export default function Crossword() {
+  return (
+    <CrosswordDiv>
+      <></>
+    </CrosswordDiv>
+  );
+}
